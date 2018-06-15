122061135fbf4689ebd63858078072c04ae570e3
ntoro <ntoro@ebi.ac.uk>
2018-06-15 16:34:03 +0100

    Adds extra info to the configuration

--- src/main/java/uk/ac/ebi/intact/portal/indexer/config/Neo4jConfiguration.java
+++ src/main/java/uk/ac/ebi/intact/portal/indexer/config/Neo4jConfiguration.java
@@ -1,5 +1,6 @@
 package uk.ac.ebi.intact.portal.indexer.config;
 
+
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
