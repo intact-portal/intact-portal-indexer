a5e191bf99e6c868a67cebe38856e80b55ade5f7
anjali <anjali@ebi.ac.uk>
2018-07-30 16:29:33 +0100

    NIV-155 added unit testing for interaction indexing

--- src/test/java/uk/ac/ebi/intact/portal/indexer/integration/InteractorsIndexerTaskletIntegrationTest.java
+++ src/test/java/uk/ac/ebi/intact/portal/indexer/integration/InteractorsIndexerTaskletIntegrationTest.java
@@ -1,13 +1,17 @@
 package uk.ac.ebi.intact.portal.indexer.integration;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.batch.core.StepContribution;
-import org.springframework.batch.core.StepExecution;
+import org.springframework.batch.core.*;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.scope.context.StepContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.test.JobLauncherTestUtils;
 import org.springframework.batch.test.StepScopeTestExecutionListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
@@ -30,22 +34,80 @@ import static org.springframework.batch.test.MetaDataInstanceFactory.createStepE
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest
-@ContextConfiguration(classes = {BatchConfiguration.class})
-@TestExecutionListeners(listeners = {
-        DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class,
-        StepScopeTestExecutionListener.class})
 //TODO Review the configuration of the test to be sure that used the localhost, in memory resources
 public class InteractorsIndexerTaskletIntegrationTest {
+
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Resource
+    private Tasklet indexCleanerTasklet;
 
     @Resource
     private Tasklet interactorIndexerTasklet;
 
+    @Autowired
+    private JobLauncher jobLauncher;
+
+    @Autowired
+    private JobRepository jobRepository;
+
+    @Autowired
+    private Job interactorIndexerJob;
+
+    private void initializeJobLauncherTestUtils(){
+        this.jobLauncherTestUtils = new JobLauncherTestUtils();
+        this.jobLauncherTestUtils.setJobLauncher(jobLauncher);
+        this.jobLauncherTestUtils.setJobRepository(jobRepository);
+        this.jobLauncherTestUtils.setJob(interactorIndexerJob);
+    }
+
     @Test
-    @DirtiesContext
-    @Transactional(readOnly = true)
-    public void testTasklet() throws Exception {
+    public void jobSimulation() throws Exception {
+
+        if(jobLauncherTestUtils==null){
+            initializeJobLauncherTestUtils();
+        }
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+        Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    public void cleanerStepSimulation() throws Exception {
+
+        if(jobLauncherTestUtils==null){
+            initializeJobLauncherTestUtils();
+        }
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("interactorCleanerStep");
+        Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    public void indexingStepSimulation() throws Exception {
+
+        if(jobLauncherTestUtils==null){
+            initializeJobLauncherTestUtils();
+        }
+        JobExecution jobExecution = jobLauncherTestUtils.launchStep("interactorIndexingStep");
+        Assert.assertEquals(jobExecution.getStatus(), BatchStatus.COMPLETED);
+
+    }
+
+    @Test
+    public void cleanerTaskletSimulation() throws Exception {
+
+        StepExecution execution = createStepExecution();
+
+        ChunkContext chunkContext = new ChunkContext(new StepContext(execution));
+        StepContribution stepContribution = new StepContribution(execution);
+
+        indexCleanerTasklet.execute(stepContribution, chunkContext);
+
+    }
+
+    @Test
+    public void indexerTaskletSimulation() throws Exception {
 
         StepExecution execution = createStepExecution();
 
