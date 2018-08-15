3ec50a5f57e4812c8412d651ee95d1cba3894ed9
anjali <anjali@ebi.ac.uk>
2018-08-15 17:10:54 +0100

    NIV-166: Added Unit Tests

--- src/main/java/utilities/CommonUtility.java
+++ src/main/java/utilities/CommonUtility.java
@@ -1,5 +1,11 @@
 package utilities;
 
+import uk.ac.ebi.intact.search.interactions.model.Interaction;
+
+import java.beans.XMLEncoder;
+import java.io.BufferedOutputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.lang.reflect.Method;
 import java.util.*;
 
@@ -8,25 +14,37 @@ import java.util.*;
  */
 public class CommonUtility {
 
-    public static Set<String> getGraphObjectsUniqueKeys(Collection<? extends Object>  graphObjects){
+    public static Set<String> getGraphObjectsUniqueKeys(Collection<? extends Object> graphObjects) {
 
-        Set<String> uniqueKeys=new HashSet<String>();
-        for(Object obj:graphObjects){
+        Set<String> uniqueKeys = new HashSet<String>();
+        for (Object obj : graphObjects) {
             Class clazz = obj.getClass();
             try {
                 Method method = clazz.getMethod("getUniqueKey");
                 String uniqueKey = (String) method.invoke(clazz.cast(obj));
                 uniqueKeys.add(uniqueKey);
-            }catch (Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
         }
-         return uniqueKeys;
+        return uniqueKeys;
     }
 
-    public static int getYearOutOfDate(Date date){
+    public static int getYearOutOfDate(Date date) {
         Calendar calendar = Calendar.getInstance();
         calendar.setTime(date);
         return calendar.get(Calendar.YEAR);
+    }
+
+    //only for testing
+    public static void saveInteractioninDisc(Collection<Interaction> interactions) {
+        XMLEncoder encoder = null;
+        try {
+            encoder = new XMLEncoder(new BufferedOutputStream(new FileOutputStream("./src/test/resources/Interactions.xml")));
+        } catch (FileNotFoundException fileNotFound) {
+            System.out.println("ERROR: While Creating or Opening the File dvd.xml");
+        }
+        encoder.writeObject(interactions);
+        encoder.close();
     }
 }
