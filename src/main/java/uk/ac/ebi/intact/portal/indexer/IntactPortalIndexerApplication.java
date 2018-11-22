2117db6afe179277806585dfdbe314c66ca7eaa2
anjali <anjali@ebi.ac.uk>
2018-11-22 13:35:17 +0000

    NIV-150: Spring Boot Exit Code, properties externalized

--- src/main/java/uk/ac/ebi/intact/portal/indexer/IntactPortalIndexerApplication.java
+++ src/main/java/uk/ac/ebi/intact/portal/indexer/IntactPortalIndexerApplication.java
@@ -2,11 +2,14 @@ package uk.ac.ebi.intact.portal.indexer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class IntactPortalIndexerApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntactPortalIndexerApplication.class, args);
+
+        ApplicationContext applicationContext=SpringApplication.run(IntactPortalIndexerApplication.class, args);
+        System.exit(SpringApplication.exit(applicationContext));
     }
 }
