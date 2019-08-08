d918944b5cf54cf0a9c4f012dcf07abf9b76c79f
anjali <anjali@ebi.ac.uk>
2019-08-08 15:31:38 +0100

    NIV-322 : Two neo4j session problem resolved for spring boot version 2.1.0, changed graphdb

--- src/main/java/uk/ac/ebi/intact/portal/indexer/config/Neo4jConfiguration.java
+++ src/main/java/uk/ac/ebi/intact/portal/indexer/config/Neo4jConfiguration.java
@@ -1,16 +1,12 @@
 package uk.ac.ebi.intact.portal.indexer.config;
 
 
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableTransactionManagement
 @ComponentScan("uk.ac.ebi.intact.graphdb")
-@EntityScan({"uk.ac.ebi.intact.graphdb.model"})
-@EnableNeo4jRepositories("uk.ac.ebi.intact.graphdb.repositories")
 public class Neo4jConfiguration {
 }
