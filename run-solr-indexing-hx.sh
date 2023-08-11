cc83d6976eed0be4821f24a12824d90fa09414ea
jmedinaebi <jmedina@ebi.ac.uk>
2023-08-11 13:04:02 +0100

    Remove unnecessary dependencies

--- run-solr-indexing-hx.sh
+++ run-solr-indexing-hx.sh
@@ -13,4 +13,4 @@
 export JAVA_HOME="/hps/software/users/hhe/intact/third-party-softwares/latest_intact_jdk11"
 export PATH="${JAVA_HOME}/bin:$PATH"
 
-mvn spring-boot:run -s settings.xml -Pip-solr-indexing-hx -Dmaven.repo.local=repository
+mvn spring-boot:run -s settings.xml -Pip-solr-indexing-hx
